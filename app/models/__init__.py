cb677c280240c9724b6345b3d1568d4a8d2206a3
Ester Gomes <ester10gomes@gmail.com>
2026-09-26 17:36:24 +0900

    import NoteType from app.models.note

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,5 +4,6 @@ from app.models.base import Base
 from app.models.user import User
 from app.models.api_client import APIClient
 from app.models.contact import Contact
-from app.models.note import Note, NoteLink
-__all__ = ["Base", "User", "APIClient", "Contact", "Note", "NoteLink"]
+from app.models.note import Note, NoteLink, NoteType
+
+__all__ = ["Base", "User", "APIClient", "Contact", "Note", "NoteLink", "NoteType"]
